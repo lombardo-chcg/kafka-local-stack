ae3dd0c2d7aefdbd34cb6a0fc61e2ad91e587783
lombardo-chcg <dpa.lombardo@gmail.com>
2017-09-04 00:03:33 -0500

    add kafka connect

--- create_readings.sh
+++ create_readings.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-endpoint="http://localhost:8080/messages"
+kafkaRestConsumer="http://localhost:8080/messages"
 
-for i in {1..99999}; do
-  reading=$(( ( RANDOM % 200 ) ))
+for i in $(seq 99999); do
+  count=0
+  while read -r; do
+    count=$(( $count + 1 ))
+    key=$(echo "$i$count")
+    lyric="$(echo -e "${REPLY}")"
 
-  curl -X POST \
-    $endpoint \
-    -H 'content-type: application/json' \
-    -d "{
-      \"topic\": \"readings\",
-      \"key\": $i,
-      \"content\": \"$reading\"
-    }"
+    # make it pretty
+    [[ $((count % 2)) -eq 0 ]] && flair='/////' || flair='\\\\\'
+    printf '%*s===>' "$count" | tr ' ' "="
+    echo ""
 
-  echo "key $i / value $reading"
+    curl -X POST \
+      $kafkaRestConsumer \
+      -H 'content-type: application/json' \
+      -d "{
+        \"topic\": \"readings\",
+        \"key\": "$key",
+        \"content\": \"$lyric\"
+      }"
+    echo "$key $flair $lyric"
+  done < <(cat data/constant_surprises.txt)
 done
